4eaa976debf61f3d0e889c488214a749d8efb2f8
anv <a.n.v@rutgers.edu>
2024-04-16 19:22:44 -0400

    update

---
+++ deepLearning/hw3/func3.py
@@ -0,0 +1,72 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import random
+import math
+import time
+
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# print('Using device:', device)
+
+class LinearSoftmaxRegression(nn.Module):
+    def __init__(self):
+        super(LinearSoftmaxRegression, self).__init__()
+
+        self.layer_1 = torch.nn.Linear( in_features = 28*28*1, out_features = 10, bias=True )
+
+    def forward(self, input_tensor):
+        flattened = nn.Flatten()( input_tensor )
+
+        logits = self.layer_1( flattened )
+
+        return logits
+
+class badVGGDec(nn.Module):
+    def __init__(self):
+        super(badVGGDec, self).__init__()
+
+        self.layer_input = nn.Sequential(nn.Linear( in_features = 28*28*1, out_features = 95, bias=True ), nn.LayerNorm(95), nn.ELU())
+        self.layer_output = torch.nn.Linear( in_features = 82, out_features = 10, bias=True )
+        self.linears = nn.ModuleList([nn.Sequential(nn.Linear(95, 90), nn.LayerNorm(90), nn.ELU()),
+                                      nn.Sequential(nn.Linear(90, 87), nn.LayerNorm(87), nn.ELU()),
+                                      nn.Sequential(nn.Linear(87, 82), nn.LayerNorm(82), nn.ELU())])
+    def forward(self, input_tensor):
+        output = nn.Flatten()( input_tensor )
+        output = self.layer_input(output)
+        for l in self.linears:
+                output = l(output)
+        output = self.layer_output(output)
+        return output
+    
+def trainNetwork(model, device, train_x, train_y, text_x, test_y, loss_function, optimFunc, batch_size, learnRate):
+    model = model.to(device)
+    optimizer = optimFunc(model.parameters(), lr = learnRate )
+    testLoss = loss_function(model(test_x),test_y).item()
+    startTime = time.time()
+    for epochs in range(30):
+        total_loss = 0
+        for batch in range( train_x.shape[0] // batch_size ):
+            x_batch, y_batch = get_batch(train_x, train_y, batch_size)
+            
+            optimizer.zero_grad()
+    
+            logits = model( x_batch )
+            loss = loss_function( logits, y_batch )
+    
+            loss.backward()
+            optimizer.step()
+    
+            total_loss += loss.item()
+    
+        print("epochs: " + str(1+epochs) + " total Loss over Batches: " + str(total_loss) + " test loss: " + str(testLoss) )
+        # if loss_function(model(test_x),test_y).item() > testLoss*1.1:
+        #     break
+        # else:
+        testLoss = loss_function(model(test_x),test_y).item()
+    endTime = time.time()
+    return endTime-startTime, loss_function(model(train_x),train_y).item(), testLoss
